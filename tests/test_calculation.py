65b7c17134c229e9ce6078c3a8aa0ddd96aa8fcb
Leonardo Alchieri <leonardo@alchieri.eu>
2022-04-28 11:38:11 +0200

    Corrected pytest implementation → I did not test it 😅

--- tests/test_calculation.py
+++ tests/test_calculation.py
@@ -1,11 +1,13 @@
 import pytest
 from numpy import ndarray, random
+from sys import path
+
+path.append(".")
 from cvxEDA import cvxEDA
 
 
-@pytest.mark.parametrize(
-    "n_array", [100, 1000, 10000, 100000], "frequency", [2, 8, 16, 32, 64]
-)
+@pytest.mark.parametrize("n_array", [100, 1000, 10000, 1000000])
+@pytest.mark.parametrize("frequency", [2, 8, 16, 32, 64])
 def test_random_eda(n_array: int, frequency: int):
     y: ndarray = random.rand(n_array)
     yn: ndarray = (y - y.mean()) / y.std()
